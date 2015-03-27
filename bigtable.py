0c5919af0923f882d0f349b470fb0802edb3ee14
Fletcher Tomalty <fletcher@tomalty.com>
2015-03-27 00:15:01 -0400

    Get rid of the optimized version of the bigtable benchmark, since the regular version is quite fast now.

--- bigtable.py
+++ bigtable.py
@@ -3,7 +3,7 @@ import timeit
 import gifruw
 
 
-TIMEIT_ROUNDS = 50
+TIMEIT_ROUNDS = 100
 TABLE = [dict(a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10) for x in range(1000)]
 
 
@@ -13,26 +13,12 @@ def bigtable():
 		for row in TABLE:
 			with d.tr():
 				for c in row.values():
-					# This would be the normal syntax:
 					d.td(c)
 	return d.render()
 
 
-def bigtable_optimized():
-	d = gifruw.Document(autoescape_mode = False)
-	with d.table():
-		for row in TABLE:
-			with d.tr():
-				for c in row.values():
-					# However, if you actually wanted to do something 10k times,
-					# you'd optimize it like this:
-					d += '<td>%s</td>' % c
-	return d.render()
+if __name__ == '__main__':
+	total_time = timeit.timeit(bigtable, number = TIMEIT_ROUNDS)
+	print "bigtable: {:.1f} ms".format(1000 * total_time / TIMEIT_ROUNDS)
 
-
-for f in [bigtable, bigtable_optimized]:
-	total_time = timeit.timeit(f, number = TIMEIT_ROUNDS)
-	print "{}: {:.1f} ms".format(f.__name__, 1000 * total_time / TIMEIT_ROUNDS)
-
-# bigtable: 61.5 ms
-# bigtable_optimized: 14.4 ms
+# bigtable: 18.9 ms
